b4c017a2e0ed6c15bee41b365971317722f33499
luctoss <luctoss@hotmail.com>
2013-02-13 10:22:07 -0500

    initial version: intro

--- exercice_0_ondart.me/0.7.dart
+++ exercice_0_ondart.me/0.7.dart
@@ -3,7 +3,5 @@ main() {
   var birth = new Date(1990, 6, 10, 12);
   var duration = now.difference(birth);
   duration = duration.inDays;
-  var years = duration / 365;
-  var mois
-  print ("${years} années, soit ${duration} jours, soit $);
+  print ("${duration} jours");
 }
